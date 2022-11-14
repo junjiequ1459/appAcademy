d41b943ff7d1c3c964bbe60ad054bdf25fde414c
RexQu <junjiequ123@gmail.com>
2022-11-14 11:53:45 -0500

    Classes in ruby

---
+++ W2D1/dog_project/lib/dog.rb
@@ -0,0 +1,46 @@
+class Dog
+  def initialize(name, breed, age, bark, favorite_foods)
+    @name = name
+    @breed = breed
+    @age = age
+    @bark = bark
+    @favorite_foods = favorite_foods
+  end
+
+  #getter name
+  def name
+    @name
+  end
+
+  #getter breed
+  def breed
+    @breed
+  end
+
+  #getter age
+  def age
+    @age
+  end
+
+  #setter age
+  def age=(num)
+    @age = num
+  end
+
+  def bark
+    if age > 3
+      return @bark.upcase
+    end
+    @bark.downcase
+  end
+
+  def favorite_foods
+    @favorite_foods
+  end
+
+  def favorite_food?(str)
+    @favorite_foods.any? { |e| e.downcase == str.downcase }
+  end
+end
+
+dog1 = Dog.new("Fido", "German Shepard", 3, "Bork!", ["Bacon", "Chicken"])
