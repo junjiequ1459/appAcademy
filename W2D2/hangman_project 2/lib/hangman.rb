8cf060c2318f9379f12e50581b7644baec0fa2ea
rexqu <junjiequ123@gmail.com>
2022-11-18 20:08:35 -0500

    hangman

--- W2D2/hangman_project 2/lib/hangman.rb
+++ W2D2/hangman_project 2/lib/hangman.rb
@@ -1,16 +1,18 @@
 class Hangman
   attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
-  def random_word
-     DICTIONARY.sample
+  def self.random_word
+    DICTIONARY.sample
   end
+  p
 
   def initialize
-    @secret_word = random_word
-    @guess_word = Array.new(@secret_word.length) {"_"}
+    @secret_word = self.random_word
+    @guess_word = Array.new(@secret_word.length) { "_" }
     @attempted_chars = []
-    @remaining_incorrect_guesses = 5 
+    @remaining_incorrect_guesses = 5
   end
+
   def already_attempted?(char)
     return true if attempted_chars.include? char
     false
@@ -18,12 +20,11 @@ class Hangman
 
   def get_matching_indices(char)
     result = []
-    @secret_word.each_char.with_index {|e,i| result << i if char == e}
+    @secret_word.each_char.with_index { |e, i| result << i if char == e }
     result
   end
 
-  def fill_indices(char,arr)
-    arr.each {|i| guess_word[i] = char}
+  def fill_indices(char, arr)
+    arr.each { |i| guess_word[i] = char }
   end
-
 end
